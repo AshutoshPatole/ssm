f833a24f7e0dc800d102424e9cf7c29505ae1dfc
AshutoshPatole <apatole479@gmail.com>
2026-08-17 17:35:42 +0530

    add connect cmd

---
+++ cmd/connect.go
@@ -0,0 +1,138 @@
+// Package cmd /*
+package cmd
+
+import (
+	"fmt"
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/TwiN/go-color"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"ssm-v2/internal/ssh"
+	"ssm-v2/internal/store"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+type serverOption struct {
+	Label       string
+	Environment string
+	HostName    string
+	User        string
+	IP          string
+}
+
+var filterEnvironment string
+
+// connectCmd represents the connect command
+var connectCmd = &cobra.Command{
+	Use:   "connect",
+	Short: "Connect to the servers",
+	Long: `
+To connect to the servers use:
+ssm connect group-name
+
+You can also specify which environments to list:
+ssm connect group-name -e ppd
+	`,
+	Aliases: []string{"c", "con"},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 || len(args) > 1 {
+			fmt.Println(color.InYellow("Usage: ssm connect group-name\nYou can also pass environment using -e (optional)"))
+			os.Exit(1)
+		}
+		return nil
+	},
+	Run: func(cmd *cobra.Command, args []string) {
+		ListToConnectServers(args[0], filterEnvironment)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(connectCmd)
+	connectCmd.Flags().StringVarP(&filterEnvironment, "filter", "f", "", "filter list by environment")
+}
+
+func ListToConnectServers(group, environment string) {
+	var config store.Config
+
+	if err := viper.Unmarshal(&config); err != nil {
+		log.Fatalln(err)
+	}
+
+	selectedEnvName := ""
+	selectedHostName := ""
+	selectedHostIP := ""
+
+	var serverOptions []serverOption
+	user := ""
+
+	for _, grp := range config.Groups {
+		if grp.Name == group {
+			user = grp.User
+			for _, env := range grp.Environment {
+				if environment != "" {
+					if environment == env.Name {
+						for _, server := range env.Servers {
+							serverOption := serverOption{
+								Label:       fmt.Sprintf("%s (%s)", server.Alias, env.Name),
+								Environment: env.Name,
+								HostName:    server.HostName,
+								IP:          server.IP,
+								User:        user,
+							}
+							serverOptions = append(serverOptions, serverOption)
+						}
+					}
+				} else {
+					for _, server := range env.Servers {
+						serverOption := serverOption{
+							Label:       fmt.Sprintf("%s (%s)", server.Alias, env.Name),
+							Environment: env.Name,
+							HostName:    server.HostName,
+							IP:          server.IP,
+						}
+						serverOptions = append(serverOptions, serverOption)
+					}
+				}
+			}
+		}
+	}
+	labels := make([]string, len(serverOptions))
+	for i, serverOption := range serverOptions {
+		labels[i] = serverOption.Label
+	}
+
+	fmt.Println(color.InGreen(selectedEnvName))
+	prompt := &survey.Select{
+		Message: "Select server",
+		Options: labels,
+	}
+	err := survey.AskOne(prompt, &selectedHostName)
+	if err != nil {
+		return
+	}
+
+	// Extract environment name from the selected option
+	for _, serverOption := range serverOptions {
+		if serverOption.Label == selectedHostName {
+			selectedEnvName = serverOption.Environment
+			selectedHostName = strings.Split(serverOption.HostName, " (")[0]
+			selectedHostIP = serverOption.IP
+			break
+		}
+	}
+	if selectedHostName != "" && user != "" && selectedEnvName != "" {
+		longestLabelLength := 12
+		colonWidth := 2
+		fmt.Println(color.InGreen(fmt.Sprintf("%-*s: %*s", longestLabelLength, "Host", colonWidth, selectedHostName)))
+		fmt.Println(color.InGreen(fmt.Sprintf("%-*s: %*s", longestLabelLength, "IP Address", colonWidth, selectedHostIP)))
+		fmt.Println(color.InGreen(fmt.Sprintf("%-*s: %*s", longestLabelLength, "User", colonWidth, user)))
+		fmt.Println(color.InGreen(fmt.Sprintf("%-*s: %*s", longestLabelLength, "Environment", colonWidth, selectedEnvName)))
+
+		ssh.Connect(user, selectedHostIP, selectedEnvName)
+	} else {
+		fmt.Println(color.InRed("Aborted! Bad Request"))
+	}
+}
---
+++ internal/ssh/connect.go
@@ -0,0 +1,55 @@
+package ssh
+
+import (
+	"errors"
+	"github.com/TwiN/go-color"
+	"github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+	"syscall"
+)
+
+func Connect(user, server, environment string) {
+	homeDir, _ := os.UserHomeDir()
+	privateKey := filepath.Join(homeDir, ".ssh/id_ed25519")
+
+	_, err := os.Stat(privateKey)
+	if os.IsNotExist(err) {
+		logrus.Fatal(color.InRed("ED25519 private key does not exists on the local system"))
+	}
+	var sshCmd *exec.Cmd
+	switch runtime.GOOS {
+	case "linux":
+		sshCmd = exec.Command("ssh", user+"@"+server)
+	case "darwin":
+		sshCmd = exec.Command("ssh", user+"@"+server)
+	case "windows":
+		logrus.Error(color.InRed("SSH connection not implemented for Windows systems yet"))
+		return
+	default:
+		logrus.Error(color.InRed("Unsupported operating system"))
+		return
+	}
+
+	sshCmd.Stdin = os.Stdin
+	sshCmd.Stdout = os.Stdout
+	sshCmd.Stderr = os.Stderr
+
+	err = sshCmd.Start()
+	if err != nil {
+		logrus.Fatal("Failed to start SSH command:", err)
+	}
+
+	err = sshCmd.Wait()
+	if err != nil {
+		var exitErr *exec.ExitError
+		ok := errors.As(err, &exitErr)
+		if !ok {
+			logrus.Fatal("Failed to wait for SSH command:", err)
+		}
+		waitStatus := exitErr.Sys().(syscall.WaitStatus)
+		logrus.Println("SSH session exited with:", waitStatus.ExitStatus())
+	}
+}
