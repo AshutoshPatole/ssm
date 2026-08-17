3624a686f887a10ce2d3d1e54b5038b9256a3dff
AshutoshPatole <apatole479@gmail.com>
2026-08-17 17:36:03 +0530

    refactor: encyrpt, decrypt logic

---
+++ internal/security/encryption.go
@@ -0,0 +1,31 @@
+package security
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/sha256"
+	"encoding/hex"
+	"log"
+)
+
+func GenerateEncryptionKey(password string) []byte {
+	hash := sha256.Sum256([]byte(password))
+	return hash[:]
+}
+
+func EncryptData(data []byte, key []byte) string {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		log.Fatalf("error creating cipher: %v", err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		log.Fatalf("error creating GCM: %v", err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	ciphertext := gcm.Seal(nonce, nonce, data, nil)
+
+	return hex.EncodeToString(ciphertext)
+}
