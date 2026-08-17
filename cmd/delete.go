be7d236f066ef88748b405b1597f40bc2adf7547
AshutoshPatole <apatole479@gmail.com>
2026-08-17 17:35:41 +0530

    add clean configuration flag in delete

--- cmd/delete.go
+++ cmd/delete.go
@@ -7,7 +7,6 @@ import (
 	"github.com/TwiN/go-color"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
-	"log"
 	"os"
 	"ssm-v2/internal/store"
 	"strings"
@@ -15,18 +14,17 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var serverToDelete string
+var (
+	serverToDelete string
+	cleanConfig    bool
+)
 
 // deleteCmd represents the delete command
 var deleteCmd = &cobra.Command{
 	Use:   "delete",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Delete a server from the configuration",
+	Long: `Delete a server from the configuration. This command will remove a server by its name
+and can optionally clean up empty groups and environments.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		deleteServer()
 	},
@@ -35,7 +33,27 @@ to quickly create a Cobra application.`,
 func init() {
 	rootCmd.AddCommand(deleteCmd)
 	deleteCmd.Flags().StringVarP(&serverToDelete, "server", "s", "", "server to delete")
+	deleteCmd.Flags().BoolVarP(&cleanConfig, "clean-config", "c", false, "Clean unused groups")
 	_ = deleteCmd.MarkFlagRequired("serverToDelete")
+}
+
+func cleanConfiguration(config *store.Config) {
+	for gi := len(config.Groups) - 1; gi >= 0; gi-- {
+		group := &config.Groups[gi]
+		for ei := len(group.Environment) - 1; ei >= 0; ei-- {
+			env := &group.Environment[ei]
+			if len(env.Servers) == 0 {
+				group.Environment = append(group.Environment[:ei], group.Environment[ei+1:]...)
+				logrus.Debugln("Removed empty environment: %s", env.Name)
+			} else {
+				logrus.Debugln("Nothing to clean")
+			}
+		}
+		if len(group.Environment) == 0 {
+			config.Groups = append(config.Groups[:gi], config.Groups[gi+1:]...)
+			logrus.Debugln("Removed empty group: %s", group.Name)
+		}
+	}
 }
 
 func deleteServer() {
@@ -53,21 +71,21 @@ func deleteServer() {
 			for si, srv := range env.Servers {
 				if srv.HostName == serverToDelete {
 
-					fmt.Println(color.InBlackOverRed(srv.HostName + " found in " + env.Name + " in " + grp.Name))
+					logrus.Info(color.InBlackOverRed(srv.HostName + " found in " + env.Name + " in " + grp.Name))
 					reader := bufio.NewReader(os.Stdin)
 					fmt.Print("Are you sure you want to delete this server? (y/n): ")
 					response, err := reader.ReadString('\n')
 					if err != nil {
-						log.Fatalln(err)
+						logrus.Fatalln(err)
 					}
 
 					response = strings.TrimSpace(response)
 					if response == "y" || response == "yes" {
 						serverFound = true
 						config.Groups[gi].Environment[ei].Servers = append(env.Servers[:si], env.Servers[si+1:]...)
-						fmt.Println(color.InGreen("Server deleted successfully!"))
+						logrus.Info(color.InGreen("Server deleted successfully!"))
 					} else {
-						fmt.Println(color.InYellow("Server deletion aborted."))
+						logrus.Info(color.InYellow("Server deletion aborted."))
 					}
 					break
 				}
@@ -78,10 +96,14 @@ func deleteServer() {
 	if !serverFound {
 		fmt.Println(color.InRed("Server " + serverToDelete + " was not found in configuration"))
 	}
-	if serverFound {
-		viper.Set("groups", config.Groups)
-		if err := viper.WriteConfig(); err != nil {
-			log.Fatalln(err)
-		}
+
+	if cleanConfig {
+		fmt.Println(color.InGreenOverBlack("Cleaning configuration"))
+		cleanConfiguration(&config)
+	}
+
+	viper.Set("groups", config.Groups)
+	if err := viper.WriteConfig(); err != nil {
+		logrus.Fatal(err)
 	}
 }
